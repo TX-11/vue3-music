21e455f8dd0a5764c163212abbfed862e7b1bd8d
tangxin <1156170865@qq.com>
2022-11-15 09:30:11 +0800

    手机验证码

--- src/components/layout/header/UserInfo.vue
+++ src/components/layout/header/UserInfo.vue
@@ -1,46 +1,44 @@
 <template>
-  <div class="flex items-center cursor-pointer hover-text">
-    <ElAvatar size="small" round class="bg-gray-200" :src="profile?.avatarUrl ?? ''"></ElAvatar>
-    <span class="text-xs ml-1.5" v-if="isLogin">{{ profile.nickname }}</span>
-    <span class="text-xs ml-1.5" @click="showLogin=true" v-else>点击登录</span>
+	<div class="flex items-center cursor-pointer hover-text">
+		<ElAvatar size="small" round class="bg-gray-200" :src="profile?.avatarUrl ?? ''"></ElAvatar>
+		<span class="text-xs ml-1.5" v-if="isLogin">{{ profile.nickname }}</span>
+		<span class="text-xs ml-1.5" @click="showLogin = true" v-else>点击登录</span>
+	</div>
+	<el-dialog v-model="showLogin" title="登录" width="330px" append-to-body>
+		<div>
+			<el-input size="large" placeholder="手机号码" :prefix-icon="Phone" v-model="phone" />
+			<button @click="sentCtcode">{{ '获取验证码' }}</button>
+			<el-input size="large" class="mt-5" placeholder="验证码" :prefix-icon="Lock" v-model="ctcode" />
 
-  </div>
-  <el-dialog v-model="showLogin" title="登录" width="330px" append-to-body>
-    <div>
-      <el-input size="large" placeholder="手机号码" :prefix-icon="Phone" v-model="phone"/>
-      <el-input size="large" class="mt-5" placeholder="登录密码" :prefix-icon="Lock" v-model="password"/>
-
-      <button @click="loginSubmit" class="button w-full mt-5 py-5" style="border-radius: 5px;">登录</button>
-    </div>
-  </el-dialog>
+			<button @click="loginSubmit" class="button w-full mt-5 py-5" style="border-radius: 5px">登录</button>
+		</div>
+	</el-dialog>
 </template>
 
 <script setup lang="ts">
-import {Lock, Phone} from '@icon-park/vue-next'
-import {useRouter} from "vue-router";
-import {onMounted, ref} from "vue";
-import {useUserStore} from "@/stores/user";
-import {storeToRefs} from "pinia";
+import { Lock, Phone } from '@icon-park/vue-next';
+import { useRouter } from 'vue-router';
+import { onMounted, ref } from 'vue';
+import { useUserStore } from '@/stores/user';
+import { storeToRefs } from 'pinia';
+import { getPhoneCaptcha, verifyCaptcha } from '@/utils/api';
 
+const router = useRouter();
 
-const router = useRouter()
-
-const phone = ref('')
-const password = ref('')
-const {login, checkLogin} = useUserStore()
-const {isLogin, profile, showLogin} = storeToRefs(useUserStore())
-
+const phone = ref('');
+const ctcode = ref('');
+const { login, checkLogin } = useUserStore();
+const { isLogin, profile, showLogin } = storeToRefs(useUserStore());
+const sentCtcode = () => {
+	getPhoneCaptcha(phone.value);
+};
 const loginSubmit = () => {
-  login(phone.value, password.value)
-}
-
+	verifyCaptcha(phone.value, ctcode.value);
+};
 
 onMounted(() => {
-  checkLogin()
-})
-
+	checkLogin();
+});
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
